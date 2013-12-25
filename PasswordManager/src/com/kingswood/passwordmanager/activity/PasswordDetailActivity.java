28b7841c5f53bfb10827d30c5c9f29e74afc01da
kingswood <kingswood0912cn@gmail.com>
2013-12-25 14:13:07 +0800

    1.Display back button on password detail activity 2.Add listener to back button and cancel button 3.Remove image buttons on password list activity

--- PasswordManager/src/com/kingswood/passwordmanager/activity/PasswordDetailActivity.java
+++ PasswordManager/src/com/kingswood/passwordmanager/activity/PasswordDetailActivity.java
@@ -6,8 +6,13 @@ import com.kingswood.passwordmanager.persistent.IPasswordDAO;
 import com.kingswood.passwordmanager.persistent.PasswordDAO;
 import com.kingswood.passwordmanager.util.PMLog;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class PasswordDetailActivity extends Activity {
@@ -19,6 +24,10 @@ public class PasswordDetailActivity extends Activity {
 	private EditText password;
 
 	private EditText description;
+	
+	private Button saveButton;
+	
+	private Button cancelButton;
 
 	public void onCreate(Bundle savedInstanceState) {
 
@@ -32,6 +41,8 @@ public class PasswordDetailActivity extends Activity {
 		username = (EditText) findViewById(R.id.text_field_username);
 		password = (EditText) findViewById(R.id.text_field_password);
 		description = (EditText) findViewById(R.id.text_field_description);
+		saveButton = (Button) findViewById(R.id.save_button);
+		cancelButton = (Button) findViewById(R.id.cancel_button);
 
 		Bundle extras = getIntent().getExtras();
 		String passwordName = extras.getString(PMConstants.PARAM_NAME);
@@ -44,7 +55,32 @@ public class PasswordDetailActivity extends Activity {
 		username.setText(vo.getUsername());
 		password.setText(vo.getPassword());
 		description.setText(vo.getDescription());
+		
+		// display back button on the bar
+		ActionBar actionBar = getActionBar();
+		actionBar.setHomeButtonEnabled(true);
+		
+		// add button listener to cancel button to display password list acitivity when the button is clicked
+		cancelButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+				backToPasswordListActivity();
+			}
+		});
 
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		return backToPasswordListActivity();
+	}
+	
+	public boolean backToPasswordListActivity(){
+		Intent intent = new Intent(this.getApplicationContext(), ListPasswordActivity.class);
+		startActivity(intent);
+		
+		return true;
 	}
 
 }
