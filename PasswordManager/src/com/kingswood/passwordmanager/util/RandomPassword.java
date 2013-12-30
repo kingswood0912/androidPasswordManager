fd042f33e7b09b1a47d11d0a8a6dcb602525a254
kingswood <kingswood0912cn@gmail.com>
2013-12-30 16:44:15 +0800

    Random password

---
+++ PasswordManager/src/com/kingswood/passwordmanager/util/RandomPassword.java
@@ -0,0 +1,28 @@
+package com.kingswood.passwordmanager.util;
+
+import java.util.Random;
+
+public class RandomPassword {
+	
+	private static final String scope = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+	public static String random(int digit){
+		
+		char[] password = new char[digit];
+		
+		char[] sourceChars = scope.toCharArray();
+		
+		int sourceLength = sourceChars.length;
+		
+		Random random = new Random();
+		
+		for(int i = 0;i<digit;i++){
+			password[i] = sourceChars[random.nextInt(sourceLength)];
+		}
+		
+		
+		return password.toString();
+		
+	}
+	
+}
