b3fcc4e7c75b8eecec333dc7e265ac368aaf6f31
kingswood <kingswood0912cn@gmail.com>
2014-01-09 16:35:47 +0800

    Adding background on lock page

---
+++ PasswordManager/src/com/kingswood/passwordmanager/activity/LockScreenActivity.java
@@ -0,0 +1,18 @@
+package com.kingswood.passwordmanager.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.kingswood.passwordmanager.R;
+
+public class LockScreenActivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.lock_screen);
+		
+	}
+	
+}
